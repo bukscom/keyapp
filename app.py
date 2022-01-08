066d63559f351219dc19aaad60d56a79c2fa5a8c
Rohan Bokadiya <82446497+bukscom@users.noreply.github.com>
2022-01-08 12:07:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def game():
             
             return redirect("https://www.websudoku.com/")
         else:
-            return "invalid key"
+            return "<center><h1>Invalid key</h1></center>"
        
 
     return render_template('index.html')
@@ -26,4 +26,4 @@ def game():
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(debug = False)
